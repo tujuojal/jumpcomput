b421d51fda18cc45132b0c950bf6f37ca1630dd5
tuomo <ojala@omini.(none)>
2012-08-24 11:57:58 +0300

    now correct?

--- lahti/inrun.py
+++ lahti/inrun.py
@@ -11,12 +11,12 @@ def inrun(kalku,valku,sxalku,syalku):
 	steps=100
 	dt=5.0/steps
 	#constants
-	D=0.25	#airresistance crossectional constant
+	D=1.3	#airresistance crossectional constant
 	g=9.81	#gravity
 	m=80	#average mass of rider
-	C=0.055	#friction coefficient
+	C=0.55	#friction coefficient
 	A=D/m#	#airresistant coefficient
-	rinnekulma=31.0*2.0*pi/360.0 #angle of the slope, constant 31, maybe a function of dx at some point
+	rinnekulma=kalku*2.0*pi/360.0 #angle of the slope, constant 31, maybe a function of dx at some point
 	#initialize with
 	#zeros
 	#direction positive is upwards (and right) 
@@ -35,23 +35,24 @@ def inrun(kalku,valku,sxalku,syalku):
 	#forward stepping solution with finite differences for speed  
 	for i in range(len(t)-1):
 		t[i+1,0]=t[i,0]+dt
-		ax[i+1,0]=-sqrt(vx[i,0]**2+vy[i,0]**2)*abs(vx[i,0])*A + g*cos(rinnekulma)*sin(rinnekulma)
+		ax[i+1,0]=-sqrt(vx[i,0]**2+vy[i,0]**2)*vx[i,0]*A + g*cos(rinnekulma)*sin(rinnekulma)
 		-g*C*cos(rinnekulma)*cos(rinnekulma)
 		ay[i+1,0]=-g-sqrt(vy[i,0]**2+vx[i,0]**2)*vy[i,0]*A + g*cos(rinnekulma)*cos(rinnekulma)
-		-g*C*cos(rinnekulma)*sin(rinnekulma)
+		+g*C*cos(rinnekulma)*sin(rinnekulma)
 		vx[i+1,0]=dt*ax[i+1,0]+vx[i,0]
 		vy[i+1,0]=dt*ay[i+1,0]+vy[i,0]
 		sx[i+1,0]=dt*vx[i+1,0]+sx[i,0]
 		sy[i+1,0]=dt*vy[i+1,0]+sy[i,0]
-		
+		if sy[i+1,0]<syalku-25:
+			break	
 	
-	pylab.plot(ax,ay)
-	#pylab.show()
 	return [t,sx,sy,vx,vy,ax,ay]
 # include this trick
 
-#if __name__ == '__main__': 
+if __name__ == '__main__': 
 #rest after
-[t,sx,sy,vx,vy,ax,ay]=inrun(-31,0,0,0)
-print sqrt(vx**2+vy**2)
-print sqrt(sx**2+sy**2)
+	[t,sx,sy,vx,vy,ax,ay]=inrun(31,0,0,0)
+	pylab.plot(sx,sy)
+	pylab.show()
+	print sqrt(vx**2+vy**2)
+	print sqrt(sx**2+sy**2)
--- lahti/lento.py
+++ lahti/lento.py
@@ -9,9 +9,10 @@ def lento(kalku,valku,sxalku,syalku):
 	#lasketaan lentorata
 	#time steps size, 100seconds / how many steps
 	steps=100
-	dt=2.5/steps
+	dt=15.5/steps
 	#constants
-	D=0.25	#airresistance crossectional constant
+	D=1.3	# airresistance crossectional constant F_d=1/2*ro*v^2*c_d*A
+	#where ro=density 1.225, A=crossectional area 1.5 m^2, c_d = drag coeff 1.5 
 	g=9.81	#gravity
 	m=80	#average mass of rider
 	C=0.055	#friction coefficient
@@ -34,21 +35,21 @@ def lento(kalku,valku,sxalku,syalku):
 	for i in range(len(t)-1):
 		t[i+1,0]=t[i,0]+dt
 		ax[i+1,0]=-sqrt(vx[i,0]**2+vy[i,0]**2)*vx[i,0]*A
-		ay[i+1,0]=-g-sign(vy[i,0])*sqrt(vy[i,0]**2+vx[i,0]**2)*vy[i,0]*A
+		ay[i+1,0]=-g-sqrt(vy[i,0]**2+vx[i,0]**2)*vy[i,0]*A
 		vx[i+1,0]=dt*ax[i+1,0]+vx[i,0]
 		vy[i+1,0]=dt*ay[i+1,0]+vy[i,0]
 		sx[i+1,0]=dt*vx[i+1,0]+sx[i,0]
 		sy[i+1,0]=dt*vy[i+1,0]+sy[i,0]
 		
-	
-	pylab.plot(sx+sxalku,sy+syalku)
-	pylab.show()
 	return [t,sx,sy,vx,vy,ax,ay]
 
 # include this trick
 
-#if __name__ == '__main__': 
+if __name__ == '__main__': 
 #rest after
-[t,sx,sy,vx,vy,ax,ay]=inrun.inrun(45,10,0,0)
-[t1,sx1,sy1,vx1,vy1,ax1,ay1]=lento(45,3,0,0)
-print vx
+	#[t,sx,sy,vx,vy,ax,ay]=inrun.inrun(45,10,0,0)
+	[t1,sx1,sy1,vx1,vy1,ax1,ay1]=lento(0,0,0,0)
+	
+	pylab.plot(sx1,sy1)
+	pylab.show()
+	print vy1
