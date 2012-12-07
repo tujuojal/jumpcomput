28959e0739243732e8411a6a2653f7f0cc2e47df
Ylijumala <tuomo.ojala@gmail.com>
2012-12-07 15:43:19 +0200

    now some more functionality
    that actually does not function anyhow

--- flask/app.py
+++ flask/app.py
@@ -16,37 +16,38 @@ import numpy
 import lento2
 import inrun3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, render_template, url_for
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
 
 app = Flask(__name__)
+app.angle=25.*2*numpy.pi/360.
+
+
 
 @app.route("/")
 def simple():
-    data_uri = open("/plot.png", "rb").read().encode("base64").replace("\n", "")
-    img_tag = '<img alt="sample" src="data:image/png;base64,{0}">'.format(data_uri)
-    print img_tag
+    return render_template('default.html')
 
 
 
 
 @app.route('/plot.png')
-def plot():
+def plot(angle=25.):
     fig = Figure()
     axis = fig.add_subplot(1, 1, 1)
-    runangle=25.*2.*numpy.pi/360.
+    runangle=angle*2.*numpy.pi/360.
     ylengthstr=20.
     radius=20.
-    ir=inrun3.Inrun()
-    ir.inrun()
-    kode=ir.takeoff2()
+    app.ir=inrun3.Inrun()
+    app.ir.inrun()
+    kode=app.ir.takeoff2()
 
-    sxloppu=ir.sx[kode]
-    syloppu=ir.sy[kode]
-    vxloppu=ir.vx[kode]
-    vyloppu=ir.vy[kode]
+    sxloppu=app.ir.sx[kode]
+    syloppu=app.ir.sy[kode]
+    vxloppu=app.ir.vx[kode]
+    vyloppu=app.ir.vy[kode]
 
     lent=lento2.Lento(sxloppu,syloppu,vxloppu,vyloppu)
     lent.laske(sxloppu,syloppu,vxloppu,vyloppu)
@@ -56,13 +57,30 @@ def plot():
     ys = lent.sy
 
     axis.plot(xs, ys)
-    axis.plot(ir.sx[:kode], ir.sy[:kode])
+    axis.plot(app.ir.sx[:kode], app.ir.sy[:kode])
     canvas = FigureCanvas(fig)
     output = StringIO.StringIO()
     canvas.print_png(output)
     response = make_response(output.getvalue())
     response.mimetype = 'image/png'
     return response
+
+
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/plot/<float:angle>')
+def recompute(angle):
+    return plot(angle)
 
 if __name__ == '__main__':
     app.run(debug=True)
